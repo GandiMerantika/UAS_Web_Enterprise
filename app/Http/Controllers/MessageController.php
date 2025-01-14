a3637a8dbe326e4fbfbb9b154488801b8f9cfb2b
GandiMerantika <144131784+GandiMerantika@users.noreply.github.com>
2025-01-15 06:55:08 +0700

    Add files via upload

---
+++ app/Http/Controllers/MessageController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Message;
+use Illuminate\Http\Request;
+
+class MessageController extends Controller
+{
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|max:255',
+            'message' => 'required|string',
+        ]);
+
+        Message::create($request->all());
+
+        return response()->json(['message' => 'Pesan berhasil dikirim!'], 200);
+    }
+}
